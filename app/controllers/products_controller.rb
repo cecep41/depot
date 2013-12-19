6f4308f5600f50f4bfb145268e155f436f5ae99d
Cecep Barkah <cecep@41studio.com>
2013-12-19 19:17:13 +0700

    Creating an Ajax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
+  include CurrentCart
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_cart
   # GET /products
   # GET /products.json
   def index
@@ -71,4 +72,5 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
+
 end
